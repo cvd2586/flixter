c81fd24dc659417ef483fb5c277f36e85247f03a
Candace Van Doren <cvd2586@gmail.com>
2019-11-30 04:10:02 +0000

    Add permissions and payments with stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      redirect_to course_path, alert: 'You must be enrolled.'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
